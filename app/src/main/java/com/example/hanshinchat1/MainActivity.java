5be192f85da3cb2539952c4f8aaece1f6b227b79
Wongi Kim <woongi3012@gmail.com>
2023-05-30 14:00:28 +0900

    first commit

---
+++ app/src/main/java/com/example/hanshinchat1/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.hanshinchat1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+    private Button logoutBtn;
+    private Button chatBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        chatBtn = (Button) findViewById(R.id.chatBtn);
+        chatBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ChatActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        logoutBtn = (Button) findViewById(R.id.logoutBtn);
+        logoutBtn.setOnClickListener(view -> {
+            FirebaseAuth.getInstance().signOut();
+
+            startActivity(new Intent(view.getContext(),GoogleSignInActivity.class));
+            Toast.makeText(getApplicationContext(), "로그아웃 성공", Toast.LENGTH_SHORT).show();
+        });
+    }
+}
