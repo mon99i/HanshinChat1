79d7413d5fb5948c51aa9b5fff3a8831cfb0bb75
simu88 <105586812+simu88@users.noreply.github.com>
2023-06-10 00:20:09 +0900

    Update HomeActivity.java

--- app/src/main/java/com/example/hanshinchat1/HomeActivity.java
+++ app/src/main/java/com/example/hanshinchat1/HomeActivity.java
@@ -26,6 +26,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class HomeActivity extends AppCompatActivity {
+    //ex
 
     Button deleteBtn;
     Button logoutBtn2;
