49ed8f74c93be35bf4af3ad0926686379d5c7b66
Kim Wongi <dnjs6963@naver.com>
2023-08-02 16:59:10 +0900

    HomeActivity 내 주변친구, 상위 5% 매칭

--- app/src/main/java/com/example/hanshinchat1/HomeActivity.java
+++ app/src/main/java/com/example/hanshinchat1/HomeActivity.java
@@ -1,10 +1,14 @@
 package com.example.hanshinchat1;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class HomeActivity extends MainActivity {
 
@@ -15,6 +19,11 @@ public class HomeActivity extends MainActivity {
 
         checkCurrentUser();
         checkProfileExist();
+
+        Button mbtiMatching = findViewById(R.id.mbti_matching);
+        Button idealTypeMatching = findViewById(R.id.ideal_type_matching);
+        Button friendAroundMatching = findViewById(R.id.friend_around_matching);
+        Button topUserMatching = findViewById(R.id.top_user_matching);
 
         clickBoard();
         clickChat();
@@ -28,6 +37,71 @@ public class HomeActivity extends MainActivity {
         ImageButton infoBtn = findViewById(R.id.info);
 
         ImageButton menuBtn = findViewById(R.id.menu);
+
+        mbtiMatching.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        idealTypeMatching.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        friendAroundMatching.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder famDialog = new AlertDialog.Builder(v.getContext(), android.R.style.Theme_DeviceDefault_Light_Dialog);
+
+                famDialog.setMessage("동일한 지역에 거주하는 상대를 검색합니다.")
+                        .setTitle("내 주변 친구 매칭")
+                        .setPositiveButton("검색", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // 검색 눌렀을 때 기능 구현
+                            }
+                        })
+                        .setNeutralButton("취소", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Log.i("Dialog", "취소");
+                                Toast.makeText(v.getContext(), "취소", Toast.LENGTH_LONG).show();
+                            }
+                        })
+                        .setCancelable(false)
+                        .show();
+            }
+        });
+
+        topUserMatching.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder tumDialog = new AlertDialog.Builder(v.getContext(), android.R.style.Theme_DeviceDefault_Light_Dialog);
+
+                tumDialog.setMessage("상위 5% 사용자를 검색합니다.\n\n*사용자 순위는 프로필의 좋아요 갯수를 합산해 매겨집니다.")
+                        .setTitle("상위 5% 매칭")
+                        .setPositiveButton("검색", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // 검색 눌렀을 때 기능 구현
+                            }
+                        })
+                        .setNeutralButton("취소", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Log.i("Dialog", "취소");
+                                Toast.makeText(v.getContext(), "취소", Toast.LENGTH_LONG).show();
+                            }
+                        })
+                        .setCancelable(false)
+                        .show();
+            }
+        });
+
 
         menuBtn.setOnClickListener(new View.OnClickListener() {
             @Override
