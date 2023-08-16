d59ee6bd2f31e6b65f929061bbeaf5def945dc61
wjsdml50 <wjsdml50@hs.ac.kr>
2023-08-16 21:01:11 +0900

    Revert "게시판, 댓글x"
    This reverts commit 319db4690e4f403441a82a7b187f20a17585512c.

---
+++ app/src/main/java/com/example/hanshinchat1/ListActivity.java
@@ -0,0 +1,34 @@
+package com.example.hanshinchat1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ListActivity extends MainActivity {
+
+    private ListView listView;
+    private ListViewAdapter adapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.board_list);
+
+        clickMenu();
+        clickHome();
+        clickRoom();
+        clickChat();
+        clickBoard();
+        clickProfile();
+
+        adapter = new ListViewAdapter();
+
+        listView = (ListView) findViewById(R.id.listview);
+        listView.setAdapter(adapter);
+
+    }
+}
