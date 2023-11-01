c587379f5f64900f7feb69ce3d33c8dfebf5720d
Simu Kim <tlan88@naver.com>
2023-11-01 19:28:11 +0900

    log

--- app/src/main/java/com/example/hanshinchat1/HomeActivity.java
+++ app/src/main/java/com/example/hanshinchat1/HomeActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
