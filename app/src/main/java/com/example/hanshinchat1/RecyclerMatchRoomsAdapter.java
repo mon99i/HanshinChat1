32efe0e27824e9acf64905d687dd356eea5c7558
Simu Kim <tlan88@naver.com>
2023-11-10 23:44:52 +0900

    좋아요

--- app/src/main/java/com/example/hanshinchat1/RecyclerMatchRoomsAdapter.java
+++ app/src/main/java/com/example/hanshinchat1/RecyclerMatchRoomsAdapter.java
@@ -9,6 +9,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -208,6 +210,7 @@ public class RecyclerMatchRoomsAdapter extends RecyclerView.Adapter<RecyclerMatc
         View view = inflater.inflate(R.layout.show_match_dialog, null);
 
         ViewPager2 showMatchViewPager = view.findViewById(R.id.showMatchViewPager);
+        CheckBox likeCheckBox=view.findViewById(R.id.recommendLikeBox);
         ImageView cancelBtn = view.findViewById(R.id.cancel_image_view);
         Button matchBtn = view.findViewById(R.id.requestMatchBtn);
         matchBtn.setText("매칭신청");
@@ -221,8 +224,8 @@ public class RecyclerMatchRoomsAdapter extends RecyclerView.Adapter<RecyclerMatc
 
         roomTitle.setText("[" + room.getCategory() + "] " + room.getTitle());
 
-        FirebaseDatabase.getInstance().getReference().child("users").child(room.getHost())
-                .addListenerForSingleValueEvent(new ValueEventListener() {
+        DatabaseReference hostRef = FirebaseDatabase.getInstance().getReference().child("users").child(room.getHost());
+        hostRef.addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
                         if (snapshot.exists()) {
@@ -266,6 +269,42 @@ public class RecyclerMatchRoomsAdapter extends RecyclerView.Adapter<RecyclerMatc
         dialog = builder.create();
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable((Color.TRANSPARENT)));
         dialog.show();
+
+
+        likeCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+               hostRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                   @Override
+                   public void onDataChange(@NonNull DataSnapshot snapshot) {
+                       UserInfo userInfo=snapshot.getValue(UserInfo.class);
+                       Integer like=userInfo.getLike();
+                       if (isChecked) {
+                           if (like == null || like == 0) {
+                               hostRef.child("like").setValue(1);
+                           } else {
+                               like++;
+                               hostRef.child("like").setValue(like);;
+                           }
+                       } else {
+                           if (like == null || like == 0) {
+                               hostRef.child("like").setValue(1);
+                           } else {
+                               hostRef.child("like").setValue(like);;
+                           }
+                       }
+                   }
+
+                   @Override
+                   public void onCancelled(@NonNull DatabaseError error) {
+
+                   }
+               });
+
+
+            }
+        });
+
 
 
         matchBtn.setOnClickListener(new View.OnClickListener() {
